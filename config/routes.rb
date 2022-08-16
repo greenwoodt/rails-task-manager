29a913057cca37139070f9abc83ec8e43325b0d5
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-08-16 16:54:16 +0200

    wow

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # View home page and view all frome the database.
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Create a new.
+  post "tasks", to: "tasks#create"
+
+  # Update the tasks
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete the tasks
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
